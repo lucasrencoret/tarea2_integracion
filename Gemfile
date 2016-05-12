9fe70453b63117fc8c6af4bbb4da67790b48b135
Lucas Rencoret <lucasrencoret36@gmail.com>
2016-05-11 23:55:57 -0300

    instagram

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.2.6'
 #gem 'sqlite3'
 gem 'mysql'
 gem 'instagram'
+gem 'sinatra'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
   def index
         @instagram = Instagram.user_recent_media("1174377623", {:count => 1})
+        
+
+  end
+  
+  def tags
+  
+  @client = Instagram.client(:access_token => session[:access_token])
+  tags = @client.tag_search('cat')
+  
+
+  
   end
 end

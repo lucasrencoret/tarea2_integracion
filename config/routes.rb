47e782941ff2eb683dea537a58585bc20ad57977
Lucas Rencoret <lucasrencoret36@gmail.com>
2016-05-15 12:14:03 -0300

    arreglando travis

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #get 'home/index'
   root :to => "home#index"
-  #get '/instagram/tag/buscar' => 'home#tags'
+  get '/instagram/tag/buscar' => 'home#tags'
   post '/instagram/tag/buscar' => 'home#tags'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
